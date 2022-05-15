8c5adef028367f0c8d8623fd839c452453d69434
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-15 14:52:32 +0300

    initial commit

---
+++ Project_2_Guess_the_Flag/Project_2_Guess_the_Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_2_Guess_the_Flag
+//
+//  Created by Станислав on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
