27d03858aa74b74984a96d4d430215c9b414e240
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-15 17:29:56 +0300

    add countries and show random countries in button

--- Project_2_Guess_the_Flag/Project_2_Guess_the_Flag/ViewController.swift
+++ Project_2_Guess_the_Flag/Project_2_Guess_the_Flag/ViewController.swift
@@ -13,11 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
+        
+        countries = [
+            "estonia", "france", "germany", "ireland", "italy", "monaco",
+            "nigeria", "poland", "russia", "spain", "uk", "us"
+        ]
+        countries.shuffle()
+        
+        askQuestion()
     }
-
-
+    
+    func askQuestion() {
+        button1.setImage(.init(named: countries[0]), for: .normal)
+        button2.setImage(.init(named: countries[1]), for: .normal)
+        button3.setImage(.init(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        
+        title = countries[correctAnswer].uppercased()
+    }
 }
-
