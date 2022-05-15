4823a2997de336e85396840dea4ac2d6661e3a4b
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-15 12:38:00 +0300

    feat(ViewController): add data source methods

--- Project_1_Strom_Viewer/Project_1_Strom_Viewer/ViewController.swift
+++ Project_1_Strom_Viewer/Project_1_Strom_Viewer/ViewController.swift
@@ -26,4 +26,18 @@ class ViewController: UITableViewController {
         
         print(pictures)
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = pictures[indexPath.row]
+        cell.contentConfiguration = content
+        
+        return cell
+    }
 }
