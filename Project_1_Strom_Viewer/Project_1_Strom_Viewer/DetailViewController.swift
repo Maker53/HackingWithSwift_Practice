bad704b21d7686ef7f9e6840ca50fd4d72cc2aad
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-15 14:26:43 +0300

    a little visual fixes

--- Project_1_Strom_Viewer/Project_1_Strom_Viewer/DetailViewController.swift
+++ Project_1_Strom_Viewer/Project_1_Strom_Viewer/DetailViewController.swift
@@ -16,9 +16,24 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
         if let imageToLoad = selectedImage {
             imageView.image = .init(named: imageToLoad)
             print("success")
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
     }
 }
