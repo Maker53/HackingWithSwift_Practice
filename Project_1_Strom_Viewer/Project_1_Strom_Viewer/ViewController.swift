0b8e5fb4c5787e600da362f63dc7affafe1025bf
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-15 11:47:45 +0300

    initial commit

---
+++ Project_1_Strom_Viewer/Project_1_Strom_Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_1_Strom_Viewer
+//
+//  Created by Станислав on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
