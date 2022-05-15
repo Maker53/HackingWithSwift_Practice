b9dfd0cddef3c364e50828ac7a6157fa11ddfda9
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-15 13:09:43 +0300

    feat(DetailViewController): create DetailViewController

---
+++ Project_1_Strom_Viewer/Project_1_Strom_Viewer/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  Project_1_Strom_Viewer
+//
+//  Created by Станислав on 15.05.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
