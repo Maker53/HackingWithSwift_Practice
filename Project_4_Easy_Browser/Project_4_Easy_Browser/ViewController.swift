97d78cbf03b94265ae9aefd3a16e57603135e673
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-16 20:23:50 +0300

    add refresh

--- Project_4_Easy_Browser/Project_4_Easy_Browser/ViewController.swift
+++ Project_4_Easy_Browser/Project_4_Easy_Browser/ViewController.swift
@@ -24,6 +24,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
         navigationController?.navigationBar.isTranslucent = false
         navigationItem.rightBarButtonItem = .init(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        toolbarItems = [spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
