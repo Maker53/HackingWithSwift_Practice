9f4ac64332b522a1c2d6722777425756a8f1ca10
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-16 07:45:08 +0300

    initial commit

---
+++ Project_4_Easy_Browser/Project_4_Easy_Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_4_Easy_Browser
+//
+//  Created by Станислав on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
