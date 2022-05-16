82a600dcf6eaad9ed42cdefda8baefda8288289d
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-16 20:13:32 +0300

    add alert controller to choose website and open it

--- Project_4_Easy_Browser/Project_4_Easy_Browser/ViewController.swift
+++ Project_4_Easy_Browser/Project_4_Easy_Browser/ViewController.swift
@@ -21,8 +21,28 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.isTranslucent = false
+        navigationItem.rightBarButtonItem = .init(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc private func openTapped() {
+        let alertController = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(.init(title: "apple.com", style: .default, handler: openPage))
+        alertController.addAction(.init(title: "hackingwithswift.com", style: .default, handler: openPage))
+        alertController.addAction(.init(title: "Cancel", style: .cancel))
+        present(alertController, animated: true)
+    }
+    
+    private func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(.init(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 }
