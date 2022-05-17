3b4398e8db5f3a8e05fec16da8dcdfe2d329b017
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-17 07:29:06 +0300

    initial commit

---
+++ Project_5_World_Scramble/Project_5_World_Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_5_World_Scramble
+//
+//  Created by Станислав on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
