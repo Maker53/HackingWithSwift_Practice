646672d1641a27732d4742efca8bb67cb0ac40e1
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-20 07:25:00 +0300

    parse json and show data in cells

--- Project_7_Whitehouse_Petitions/Project_7_Whitehouse_Petitions/ViewController.swift
+++ Project_7_Whitehouse_Petitions/Project_7_Whitehouse_Petitions/ViewController.swift
@@ -8,9 +8,44 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let petition = petitions[indexPath.row]
+        
+        content.text = petition.title
+        content.secondaryText = petition.body
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    private func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
     }
 }
