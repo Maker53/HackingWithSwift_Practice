21fca8affdea681c9062a1bb4ecf0e8a69e52fde
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-20 06:45:29 +0300

    initial commit

---
+++ Project_7_Whitehouse_Petitions/Project_7_Whitehouse_Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_7_Whitehouse_Petitions
+//
+//  Created by Станислав on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
