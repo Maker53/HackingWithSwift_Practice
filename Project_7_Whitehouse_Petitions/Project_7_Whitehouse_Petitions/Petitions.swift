41fa380d1f34e9bcf103a227e2791466c7bc0fa2
Shalgin Stanislav <s.shalgin@gmail.com>
2022-05-20 07:11:47 +0300

    add model for parse JSON

---
+++ Project_7_Whitehouse_Petitions/Project_7_Whitehouse_Petitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project_7_Whitehouse_Petitions
+//
+//  Created by Станислав on 20.05.2022.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
